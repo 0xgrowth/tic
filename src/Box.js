0dcb65be327ca0a4079b194d2cc91d5cda0d739a
0xgrowth <sapientiakonzept@gmail.com>
2024-09-24 10:17:41 +0100

    Version three, remember css

--- src/Box.js
+++ src/Box.js
@@ -16,13 +16,30 @@ function Square({ value, onSquareClick }) {
 
 
 
-export default function Box() {
+function Box({ xIsNext, squares, onPlay }) {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i){
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        setSquares(nextSquares);
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        onPlay(nextSquares);
+    }
+
+    const win = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner:' + win;
+    } else {
+        status = 'Next Player:' + (xIsNext ? 'X' : 'O');
     }
 
     return (
@@ -44,3 +61,44 @@ export default function Box() {
     );
   }
   
+  function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+  }
+
+  export default function Game () {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState(Array(9).fill(null));
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay (nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+
+    return (
+        <div className = "game">
+        <div className = "game-board">
+        <Board />
+        </div>
+        <div className = "game-info">
+        <ol> {/* todo */}</ol>
+        </div>
+        </div>
+    )
+  }
